5cec648eb0c9eef3445e04716ee757d9a743fdca
Marko1310 <marko.cabo13@gmail.com>
2023-04-01 16:58:19 +0200

    implementing eslint

--- server.js
+++ server.js
@@ -1,21 +1,23 @@
-const express = require("express");
-const cors = require("express");
+const express = require('express');
+
+const cors = require('express');
+
 const app = express();
 
 const PORT = process.env.PORT || 8000;
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-app.use(cors());
+// app.use(cors());
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
 
-app.get("/", async (req, res) => {
+app.get('/', async (req, res) => {
   try {
-    return res.status(200).json("Hello server");
+    return res.status(200).json('Hello server');
   } catch (err) {
-    console.log(err);
+    return console.log(err);
   }
 });
