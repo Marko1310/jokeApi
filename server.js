71493ad17cd5da8f443112b4820329fd2084f6ac
Marko1310 <marko.cabo13@gmail.com>
2023-04-01 16:04:44 +0200

    first commit, setting up a server

---
+++ server.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const cors = require("express");
+const app = express();
+
+const PORT = process.env.PORT || 8000;
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(cors());
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
+
+app.get("/", async (req, res) => {
+  try {
+    return res.status(200).json("Hello server");
+  } catch (err) {
+    console.log(err);
+  }
+});
