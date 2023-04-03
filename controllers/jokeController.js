f9f1e1134b95937f76f098bb8aa0a081dcbed6fe
Marko1310 <marko.cabo13@gmail.com>
2023-04-03 17:33:12 +0200

    configuring nodemailer

--- controllers/jokeController.js
+++ controllers/jokeController.js
@@ -1,5 +1,6 @@
 const User = require('../models/user');
 const axios = require('axios');
+const nodemailer = require('nodemailer');
 
 module.exports.sendJoke = async (req, res) => {
   const user_id = req.id;
@@ -9,6 +10,30 @@ module.exports.sendJoke = async (req, res) => {
       const response = await axios.get(
         'https://api.chucknorris.io/jokes/random'
       );
+      console.log(response.data.value);
+
+      const transporter = nodemailer.createTransport({
+        service: process.env.MAIL_SERVICE,
+        auth: {
+          user: process.env.MAIL_USER,
+          pass: process.env.MAIL_PASSWORD,
+        },
+      });
+
+      const options = {
+        from: process.env.MAIL_USER,
+        to: 'marko.cabo13@gmail.com',
+        subject: 'Here is your random Chuck Norris joke',
+        text: response.data.value,
+      };
+
+      transporter.sendMail(options, (err, info) => {
+        if (err) {
+          console.log(err);
+        }
+        console.log(info.response);
+      });
+
       console.log(response.data.value);
       res.json(response.data.value);
     }
